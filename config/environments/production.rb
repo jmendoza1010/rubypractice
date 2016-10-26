e017a5aaf7e9fe3b8d5252089e209c4dd4ddf13a
jmendoza1010 <jmendoza.g6d@gmai.com>
2016-10-26 00:35:25 +0000

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added devise as per installation instructions
+  config.action_mailer.default_url_options = { host: 'https://jpindoza.herokuapp.com'}
 end
